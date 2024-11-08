f8503bd049b1fb866eb036263e07401f81baf596
TerminalDZ <boukemoucheidriss@gmail.com>
2024-11-08 18:30:54 +0100

    ussd

--- multi_send.py
+++ multi_send.py
@@ -1,18 +1,26 @@
 import serial
 import time
+from binascii import unhexlify
 
-port = 'COM10'  
+
+port = 'COM6'  
 baudrate = 9600
 
-# قائمة الأوامر التي سيتم إرسالها
 commands = [
-  #"AT+CUSD=?",  # تحقق من دعم USSD
-  #"AT+CUSD?",   # تحقق من حالة USSD
-  #"AT+CUSD=0",  # إلغاء USSD
-  #'AT+CSCS="GSM"',  # تعيين مجموعة الأحرف
-  #'AT+CUSD=1,"*100#"',  # استبدل *100# بكود USSD آخر
-  #  'AT+CUSD=1,"*500#",15'
-   'AT'
+
+'AT',
+#'AT+CREG?', 
+#'AT+COPS?', 
+#'AT+CMGL="ALL"',
+#'AT+CMGF=1',
+#'AT+CPMS',
+'AT+CSCS=?',
+'At+CSCS="IRA"',
+'AT+CSCS?',
+'AT+CUSD=1,"54C983460", 15', #AT+CUSD=1,"*200#", 15
+
+
+
 ]
 
 with serial.Serial(port, baudrate, timeout=1) as ser:
@@ -20,6 +28,9 @@ with serial.Serial(port, baudrate, timeout=1) as ser:
 
   for command in commands:
       ser.write((command + '\r\n').encode())  
-      time.sleep(1)  
+      time.sleep(3)  
       response = ser.read(ser.in_waiting or 1).decode() 
       print(f"Response for '{command}': {response}") 
+
+  ser.close()      
+
